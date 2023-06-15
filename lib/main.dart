434e13d82496769577004d9dcd9981727b8b645f
Elyasahmadi79 <elyas.ahmady1@gmail.comm>
2023-06-15 12:22:53 +0430

    Create DiceProject and make it Random

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,17 +9,53 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.red,
-          title: Text('Dice'),
+          title: const Text('Dice'),
         ),
-        body: MyApp(),
+        body: Dice(),
       ),
     ),
   );
+}class Dice extends StatefulWidget {
+  const Dice({super.key});
+
+  @override
+  State<Dice> createState() => _DiceState();
 }
 
-class MyApp extends StatelessWidget {
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changeDices() {
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                child: Image(image: AssetImage('images/dice$leftDice.png')),
+                onPressed: () {
+                  setState(() {
+                    changeDices();
+                  });
+                },
+              )),
+          Expanded(
+              child: TextButton(
+                child: Image(image: AssetImage('images/dice$rightDice.png')),
+                onPressed: () {
+                  setState(() {
+                    changeDices();
+                  });
+                },
+              )),
+        ],
+      ),
+    );
   }
 }
+
